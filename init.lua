798459707111ddc7340a4fbabfb27bf74613734f
Stephan Bischoff <stephan.bischoff@digle.net>
2021-08-16 22:24:40 +0200

    [add] download packer via lua

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- WARNING: Requires packer to be installed: https://github.com/wbthomason/packer.nvim
 
-require'settings'
 require'plugins'
+require'settings'
 require'keybindings'
 
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,24 @@
+-- INFO: from https://jdhao.github.io/2021/07/11/from_vim_plug_to_packer/
+local exec = vim.api.nvim_command
+local fn = vim.fn
+
+local packer_install_dir = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+local plug_url_format = 'https://github.com/%s'
+local packer_repo = string.format(plug_url_format, 'wbthomason/packer.nvim')
+local install_cmd = string.format('10split |term git clone --depth=1 %s %s', packer_repo, packer_install_dir)
+
+if fn.empty(fn.glob(packer_install_dir)) > 0 then
+    vim.api.nvim_echo({{'Installing packer.nvim', 'Type'}}, true, {})
+    exec(install_cmd)
+    exec 'packadd packer.nvim'
+end
+
+vim.cmd [[packadd packer.nvim]]
+-- End of Copy 'n Paste
+
+
+
 return require'packer'.startup(function()
     use 'wbthomason/packer.nvim'
 
